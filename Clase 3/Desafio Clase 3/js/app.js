e1d1a2c68d6eaabaf03fd48030dfa67448504922
LaggerP <pablo_lagger@hotmail.com>
2018-05-26 11:08:25 -0300

    Agregado de modificacion en division y multiplicación para hacer calculadora mas exacta

--- Clase 3/Desafio Clase 3/js/app.js
+++ Clase 3/Desafio Clase 3/js/app.js
@@ -9,11 +9,29 @@ function restaNumeros(numero, numero2){
 }
 
 function multiplicacionNumeros(numero, numero2){
+    while (numero === 0 || numero2 === 0){
+        if (numero === 0) {
+            var numero = prompt("Ingrese primer numero: ");
+            numero = parseInt(numero, 10);
+        } else if (numero2 === 0) {
+            var numero2 = prompt("Ingrese segundo numero: ");
+            numero2 = parseInt(numero2, 10);
+        }
+    }
     var multiplicacion = numero * numero2;
     console.log("La multiplicacion entre " + numero + " y " + numero2 + " es igual a " + multiplicacion);
 }
 
 function dividirNumeros(numero, numero2){
+    while (numero === 0 || numero2 === 0) {
+        if (numero === 0) {
+            var numero = prompt("Ingrese primer numero: ");
+            numero = parseInt(numero, 10);
+        } else if (numero2 === 0) {
+            var numero2 = prompt("Ingrese segundo numero: ");
+            numero2 = parseInt(numero2, 10);
+        }
+    }
     var division = numero / numero2;
     console.log("La division entre " + numero + " y " + numero2 + " es igual a " + division);
 }
@@ -30,11 +48,12 @@ function calculadora() {
     } else if (opcion === 2) {
         restaNumeros(numero, numero2);
     } else if (opcion === 3) {
-        multiplicacionNumeros(numero1, numero2);
+        multiplicacionNumeros(numero, numero2);
     } else if (opcion === 4) {
         dividirNumeros(numero, numero2);
     }
 }
+
 // [Violation] 'readystatechange' handler took xxx ms
 // VIOLACION GENERADA POR SOBRECARGA DE FUNCIONES
 document.onreadystatechange = function () {
